7bc5146675ce6f472f93641952b6b8c6b3a4768d
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-02-19 15:24:42 -0300

    create a fake user and create a token using jwt

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,8 +1,31 @@
 import { Request, Response } from "express";
+import { AppError } from "@/utils/AppError";
+import { authConfig } from "@/configs/auth";
+import { sign } from "jsonwebtoken";
+
+// Simular um usuário válido
 
 class SessionsController {
     async create(request: Request, response: Response) {
-        return response.status(201).json();
+        const { username, password } = request.body;
+
+        const fakeUser = {
+            id: 1,
+            username: "mateus",
+            password: "123",
+        };
+
+        // Verificando se o usuário e senha estão corretos
+        if (username !== fakeUser.username || password !== fakeUser.password) {
+            throw new AppError("Usuário e/ou senha incorreta", 401);
+        }
+        const { secret, expiresIn } = authConfig.jwt;
+        const token = sign({}, secret, {
+            expiresIn,
+            subject: String(fakeUser.id),
+        });
+
+        return response.json(token);
     }
 }
 
