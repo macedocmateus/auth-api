3236c5d660d8ceae4b95d52be05458bede20847c
Mateus Macedo <mateusmacedoeng@gmail.com>
2025-02-19 14:16:00 -0300

    testing the responses

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,7 +1,9 @@
-import { Request, Response } from "express"
+import { Request, Response } from "express";
 
 class SessionsController {
-  async create(request: Request, response: Response) {}
+    async create(request: Request, response: Response) {
+        return response.status(201).json();
+    }
 }
 
-export { SessionsController }
+export { SessionsController };
